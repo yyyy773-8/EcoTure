8aae1f3b08d669ccc19bda927985959d0274ec22
yyyy773-8 <Yuliakim523@gmail.com>
2025-06-11 13:04:30 +0300

    Create custom.js

---
+++ public/js/custom.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const forms = document.querySelectorAll('.needs-validation');
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+});
