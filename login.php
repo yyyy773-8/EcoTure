6845935288bffbabbc2e690be9f59e0c2d752d7d
yyyy773-8 <Yuliakim523@gmail.com>
2025-06-12 23:06:25 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,23 @@
-<IDOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset-"UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация и Вход</title>
 </head>
 <body>
-<form action="register.php" method="post">
-<input type="text" placeholder="login" name="login">
-<input type="text" placeholder-"password" name-"pass">
-<input type="text" placeholder="repeat password" name="repeatpass">
-<input type="text" placeholder="email" name="email">
-<button type="submit">Зарегистироваться</button>
-</form>
-<form action="login.php" method="post">
-<input type="text" placeholder="login" name="login">
-<input type="text" placeholder="password" name="pass">
-<button type="submit">Войти</button>
-</form>
+    <form action="register.php" method="post">
+        <input type="text" placeholder="login" name="login" required>
+        <input type="password" placeholder="password" name="pass" required> <!-- Исправлено на type=password -->
+        <input type="password" placeholder="repeat password" name="repeatpass" required> <!-- Исправлено на type=password -->
+        <input type="email" placeholder="email" name="email" required> <!-- Исправлено на type=email -->
+        <button type="submit">Зарегистрироваться</button>
+    </form>
+    <form action="login.php" method="post">
+        <input type="text" placeholder="login" name="login" required>
+        <input type="password" placeholder="password" name="pass" required> <!-- Исправлено на type=password -->
+        <button type="submit">Войти</button>
+    </form>
 </body>
 </html>
