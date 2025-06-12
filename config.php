91eb860f3dd7cccb74ebd5d482418874ea62c718
yyyy773-8 <Yuliakim523@gmail.com>
2025-06-12 21:54:43 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,9 @@
 <?php
-include 'config.php'; // Файл с настройками базы данных
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $email = $_POST['email'];
-
-    $stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-    if ($stmt->execute([$username, $password, $email])) {
-        echo "Registration successful!";
-    } else {
-        echo "Error: " . $stmt->errorInfo()[2];
-    }
-}
-?>
+return [
+    'db' => [
+        'host'     => 'localhost',
+        'dbname'   => 'yyyy',
+        'username' => 'root',
+        'password' => '',
+    ],
+];
