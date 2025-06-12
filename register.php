5a341f139c3f973cb9fd7a206b27115ae3839cec
yyyy773-8 <Yuliakim523@gmail.com>
2025-06-12 21:17:29 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,21 @@
 <? php
-
-   echo $_Post['login'];
+requere_once('db.php");
+$login = $_POST['login'];
+$pass = $_POST['pass'];
+$repeatpass = $_POST['repeatss'];
+$email = $_POST['email'];
+   if (empty($login)||empty($repeatpass)||empty($email)){
+   echo "Заполните все поля";
+   } else
+   {
+   if($pass !=$repeatpass){
+   echo "Пароли не совпадают";
+   } else {
+   $sql = "INCERT INTO" 'users' (login, pass, email) VALUES ('$login','$pass',
+   if ($conn -> query($ql)===TRUE){
+   echo "Успешная регистрация";
+   }
+   else
+   }
+   }
+   
