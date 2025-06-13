bab26f8f5a93302108ed78335a1fb5a0a489262f
yyyy773-8 <Yuliakim523@gmail.com>
2025-06-13 09:47:08 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,26 +1,41 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Регистрация</title>
-</head>
-<body>
-<h2>Форма регистрации:</h2>
-<form action="register.php" method="post">
-    Имя пользователя:<br><input type="text" name="username"><br>
-    Пароль:<br><input type="password" name="password"><br>
-    Подтверждение пароля:<br><input type="password" name="confirm_password"><br>
-    E-mail:<br><input type="email" name="email"><br>
-    Запомнить меня?<br><input type="checkbox" name="remember_me"><br>
-    <button type="submit">Зарегистрироваться</button>
-</form>
+<?php
+session_start();
+$host = 'localhost';
+$dbname = 'yyyy'; // Укажите название вашей БД
+$user = 'root';                 // Ваш юзер mysql
+$pass = '';                    // Ваш пароль mysql
 
-<h2>Авторизация:</h2>
-<form action="login.php" method="post">
-    Имя пользователя:<br><input type="text" name="username"><br>
-    Пароль:<br><input type="password" name="password"><br>
-    Запомнить меня?<br><input type="checkbox" name="remember_me"><br>
-    <button type="submit">Войти</button>
-</form>
-</body>
-</html>
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $rememberMe = isset($_POST['remember_me']);
+        
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE username=:username");
+        $stmt->execute(['username' => $username]);
+        $userData = $stmt->fetch();
+        
+        if (!$userData || !password_verify($password, $userData['password_hash'])) {
+            die('Неверное имя пользователя или пароль');
+        }
+        
+        session_regenerate_id(); // обновляем сессию для безопасности
+        $_SESSION['logged_in_user'] = [
+            'id' => $userData['id'],
+            'username' => $userData['username']
+        ];
+        
+        if ($rememberMe) { // Если установлен флажок "Запомнить меня"
+            setcookie('remember_me', json_encode($_SESSION), time()+60*60*24*7, '/'); // Cookie действует неделю
+        }
+        
+        header('Location: welcome.php');
+        exit();
+    }
+} catch (\PDOException $e) {
+    die("Ошибка подключения к базе данных: {$e->getMessage()}");
+}
+?>
